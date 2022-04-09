5a99f741ec25da3d2e15ae569b6d5990386a7c05
tilmana <59236083+tilmana@users.noreply.github.com>
2022-04-09 10:32:56 -0400

    Create userparse.py

---
+++ userparse.py
@@ -0,0 +1,34 @@
+import sys
+
+output = True
+allUsers = []
+
+try:
+    sys.argv[1]
+except Exception:
+    print("Syntax: python3 script.py file.txt (kerbrute userenum output)")
+    sys.exit("Error: no arguments provided!")
+try:
+    sys.argv[2]
+    sys.argv[3]
+except Exception:
+    output = False
+    pass
+f = open(sys.argv[1], "r")
+
+for line in f.readlines():
+    if "VALID USERNAME:" in line:
+        value = line[(line.find("VALID USERNAME:") + len("VALID USERNAME:")):(line.find("@"))].replace(" ", "").lower()
+        if value.lower() not in allUsers:
+            allUsers.append(value.lower())
+
+f.close()
+
+for user in allUsers:
+    print(user)
+
+try:
+    f = open(sys.argv[2], "w")
+    f.write(allUsers)
+except Exception:
+    pass
